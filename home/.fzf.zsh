120862832cc6b7b04c50bbe8d60b961fcf807cd1
DylanHalstead <dylanhalstead11@gmail.com>
2026-08-28 14:25:04 -0400

    Configure shell environments

---
+++ home/.fzf.zsh
@@ -0,0 +1,7 @@
+# Setup fzf
+# ---------
+if [ -d "$HOME/.fzf/bin" ] && [[ ! "$PATH" == *"$HOME/.fzf/bin"* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+fi
+
+command -v fzf >/dev/null 2>&1 && source <(fzf --zsh)
